bee18e144648d8da2f9935a5a48a6cd24f1c71e4
alv31415 <aleonvillares31415@gmail.com>
2021-08-04 17:49:08 +0200

    Commented EulerCromer class

--- EulerCromer.py
+++ EulerCromer.py
@@ -19,21 +19,39 @@ class EulerCromer(Integrator):
         super().__init__(nbody, steps, delta, tolerance, adaptive, c)
 
     def integration_step(self, t):
+        """
+        Integration step for the Euler-Cromer method.
+        v_t = v_{t-1} + a_{t-1}*Δt
+        x_t = x_{t-1} + v_{t}*Δt
+        :param t: the step at which the calculation is made
+        """
+
+        # check: step t is the same as the expected step int_step.
+        # Ensures that when performing an integration_step, they happen at consercutive times
+        # For example, integration_step(42) can only be performed if we have previously executed integration_step(41)
         assert self.int_step == t, f"Attempted to integrate with a discontinuous time step. \n" \
                                    f"Step to Integrate: {t}\n" \
                                    f"Expected Step to Integrate: {self.int_step}\n"
 
+        # perform Euler-Cromer step
         acc_t = self.nbody.get_acceleration()
 
         new_velocities = self.velocity_orbit[:, t-1, :] + self.delta * acc_t
         new_positions = self.position_orbit[:, t-1, :] + self.delta * new_velocities
 
+        # update the simulation with the calculated position and velocities
         self.nbody.update(new_positions, new_velocities, symplectic = True, tolerance = self.tolerance)
+
+        # add the newly calculated energies and angular momentum (and adaptive delta) to the historic arrays
         self.update_historic(t)
 
+        # if adaptive timestep is used, recalculate it
         if self.adaptive:
             self.delta = nm.variable_delta(self.nbody.positions, self.nbody.velocities, c=self.c)
 
+        # set the newly calculated positions and velocities to the orbit arrays
         self.position_orbit[:, t, :] = new_positions
         self.velocity_orbit[:, t, :] = new_velocities
+
+        # increment the int_step to ensure that integration_step is performed on continuous steps
         self.int_step = t + 1
