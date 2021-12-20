0a2b026ca21f684575b1b10f8d517d75c3962a4d
alv31415 <aleonvillares31415@gmail.com>
2021-12-20 22:10:38 +0100

    Improved settings

--- backend/nbodyBACKEND/nbodyBACKEND/settings.py
+++ backend/nbodyBACKEND/nbodyBACKEND/settings.py
@@ -26,9 +26,13 @@ with open("../../../django-key.txt") as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",
+                "127.0.0.1"]
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 # Application definition
